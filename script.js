9a1919b81c5c1ccb3a40bcc3af27cae9b5bed6b0
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-05-17 23:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 // complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
-Person.prototype.greet = function() {
-	console.log("Hello, my name is" + this.name + " and I am " + this.age +" years old.")
+ 
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
-
+ 
 function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
+ 
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
-Employee.prototype.jobGreat = function() {
-	console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
+ 
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
 };
 // var person = new Person("John", 30);
 // person.greet();
