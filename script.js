089bdd6379257e6fb08a0e62136295b6880e1fac
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-05-17 21:43:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ Employee.prototype.jobGreat = function() {
 	console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
 };
 var person = new Person("John", 30);
-Person.greet();
+person.greet();
 var employee = new Employee("Jane", 25, "Manager");
 employee.greet();
 employee.jobGreet();
